ec78747ca514e24959fe1ace87dc01661f9bbb7a
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2023-01-30 12:05:12 -0300

    commit

---
+++ conversorDeMoedas/script.js
@@ -0,0 +1,7 @@
+var nomeDoCliente = "Maria";
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.32;
+
+var valorEmReal = (30 * 5.32).toFixed(2);
+
+alert(`Olá ${nomeDoCliente}, o valor convertido em real é R$ ${valorEmReal}`);
