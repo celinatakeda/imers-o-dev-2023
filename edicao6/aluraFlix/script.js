ff34af050d3430ed2b133dae61d9c091bbdb5701
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2023-02-01 11:36:50 -0300

    Update script.js

--- edicao6/aluraFlix/script.js
+++ edicao6/aluraFlix/script.js
@@ -1,8 +1,10 @@
 
 var listaFilmes = [
-  'https://th.bing.com/th/id/OIP.XTiah15THsjODxIlYYdQrwAAAA?w=115&h=180&c=7&r=0&o=5&pid=1.7',
+  'https://th.bing.com/th/id/OIP.nQOfn9T18Oik8LSTKQYNCwAAAA?pid=ImgDet&w=207&h=296&c=7',
   'https://http2.mlstatic.com/filme-a-chegada-formato-digital-hd-D_NQ_NP_764442-MLB29011913596_122018-F.jpg',
-  'https://2.bp.blogspot.com/-fR5CGHvrvMg/XDu29X_rwsI/AAAAAAAAGJ8/uHSQAgF8PZwX_tsoMvnhqfi549Bi_N0QwCLcBGAs/s1600/aranhaverso.jpg'
+  'https://th.bing.com/th/id/OIP.B9zfeUrjM5v0GUQcXjARCwHaLH?pid=ImgDet&w=204&h=306&c=7',
+  'https://th.bing.com/th/id/OIP.Pyi5TpwoqE1YaF1s6KHy4gHaLH?pid=ImgDet&w=204&h=306&c=7',
+  'https://image.tmdb.org/t/p/w500/4RIr8hGts84JLZ8f2EoeYh4uk7.jpg'
 ] 
 
 for (var i = 0; i < listaFilmes.length; i++) {
